b0c4e973497a9c8076099368fe684cc79599ed3a
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-01-27 12:54:23 -0200

    Setup documentation build for the API

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,10 @@ require('dotenv').config(); // loads .env file
 
 const gulp = require('gulp');
 const watch = require('gulp-watch');
+const apidoc = require('gulp-apidoc');
 
 const dev = './development';
+const api_doc_path = './docs/api';
 const server = process.env.SERVER_PATH;
 
 gulp.task('copy', function() {
@@ -13,7 +15,14 @@ gulp.task('copy', function() {
         .pipe(gulp.dest(server));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function(done) {
     watch(dev+'/**/*', {path: dev})
         .pipe(gulp.dest(server));
+});
+
+gulp.task('doc', function(done) {
+    apidoc({
+        src: dev,
+        dest: api_doc_path
+    }, done);
 });
