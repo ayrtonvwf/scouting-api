970b106f9c076f60f961935b9d8419a6670ee3a9
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-01-27 15:58:18 -0200

    Use id to get the user on API

--- development/application/controllers/User.php
+++ development/application/controllers/User.php
@@ -3,15 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends MY_Controller {
 	/**
-	 * @api {get} /user Request User information
+	 * @api {get} /user/:id Request User Information
 	 * @apiName GetUser
 	 * @apiGroup User
+	 * 
+	 * @apiParam {Number} id User id.
 	 *
+	 * @apiSuccess {Number} id Id of the User.
 	 * @apiSuccess {String} name Name of the User.
 	 * @apiSuccess {String} email Email of the User.
 	 * @apiSuccess {Boolean} verified_email Whether the User has verified it's email address.
 	 * @apiSuccess {Boolean} enabled Whether the User is enabled for submiting evaluations.
 	 * @apiSuccess {DateTime} created_at Creation date of the User.
 	 */
-	public function get(){ }
+	public function get(int $id){ }
 }
