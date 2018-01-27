15008f16465a0f0a3e8e5d42e00ed6dfad01f155
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-01-27 12:54:53 -0200

    Initial documentation of the API

---
+++ docs/api/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-01-27T14:52:08.103Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
