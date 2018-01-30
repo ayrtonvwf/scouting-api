f3ef14df6aeb21b1276b4ba6354a8c38be60ce32
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-01-29 22:39:19 -0200

    Add period api documentation

--- development/application/controllers/Period.php
+++ development/application/controllers/Period.php
@@ -3,5 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Period extends MY_Controller {
 	
-	public function get(){ }
+	/**
+	 * @api {get} /period/:id Request Question information
+	 * @apiName PeriodGet
+	 * @apiGroup Period
+	 * @apiVersion 1.0.0
+	 * 
+	 * @apiParam {Number} id Period id.
+	 *
+	 * @apiSuccess {Number} id Id of the Period.
+	 * @apiSuccess {String} name Name of the Period.
+	 * @apiSuccess {Number} position Position of the Period.
+	 * @apiSuccess {DateTime} created_at Creation date of the Period.
+	 */
+	public function get(int $id){ }
+
+	/**
+	 * @api {get} /period/list Request Periods List
+	 * @apiName PeriodGetList
+	 * @apiGroup Period
+	 * @apiVersion 1.0.0
+	 * 
+	 * @apiParam {Number} [position_start] First position of the Period.
+	 * @apiParam {Number} [position_end] End position of the Period.
+	 * @apiParam {String} [search] Search string.
+	 *
+	 * @apiSuccess {Object[]} periods List of Periods
+	 * @apiSuccess {Number} periods.id Id of the Period.
+	 * @apiSuccess {String} periods.name Name of the Period.
+	 * @apiSuccess {Number} periods.position Position of the Period.
+	 * @apiSuccess {DateTime} periods.created_at Creation date of the Period.
+	 */
+	public function list(){ }
+}
 }
