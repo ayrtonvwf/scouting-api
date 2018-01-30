90a878422be43c80405f2fbca0db298853cccad5
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-01-29 22:29:25 -0200

    Add question's api documentation

--- development/application/controllers/Question.php
+++ development/application/controllers/Question.php
@@ -3,5 +3,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Question extends MY_Controller {
 	
-	public function get(){ }
+	/**
+	 * @api {get} /question/:id Request Question information
+	 * @apiName QuestionGet
+	 * @apiGroup Question
+	 * @apiVersion 1.0.0
+	 * 
+	 * @apiParam {Number} id Question id.
+	 *
+	 * @apiSuccess {Number} id Id of the Team.
+	 * @apiSuccess {Number} period_id Id of the Period of the Questions.
+	 * @apiSuccess {String} description Description of the Question.
+	 * @apiSuccess {Number} question_type_id Id of the Type of the Question.
+	 * @apiSuccess {DateTime} created_at Creation date of the Question.
+	 */
+	public function get(int $id){ }
+
+	/**
+	 * @api {get} /question/list Request Questions List
+	 * @apiName QuestionGetList
+	 * @apiGroup Question
+	 * @apiVersion 1.0.0
+	 * 
+	 * @apiParam {Number} [period_id] Id of the Period of the Questions.
+	 * @apiParam {Number} [question_type_id] Id of the Type of the Question.
+	 * @apiParam {String} [search] Search string.
+	 *
+	 * @apiSuccess {Object[]} questions List of Questions
+	 * @apiSuccess {Number} questions.id Id of the Team.
+	 * @apiSuccess {Number} questions.period_id Id of the Period of the Questions.
+	 * @apiSuccess {String} questions.description Description of the Question.
+	 * @apiSuccess {Number} questions.question_type_id Id of the Type of the Question.
+	 * @apiSuccess {DateTime} questions.created_at Creation date of the Question.
+	 */
+	public function list(){ }
 }
--- docs/api/api_data.js
+++ docs/api/api_data.js
@@ -67,6 +67,155 @@ define({ "api": [
   },
   {
     "type": "get",
+    "url": "/question/:id",
+    "title": "Request Question information",
+    "name": "QuestionGet",
+    "group": "Question",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Question id.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "id",
+            "description": "<p>Id of the Team.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "period_id",
+            "description": "<p>Id of the Period of the Questions.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "description",
+            "description": "<p>Description of the Question.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "question_type_id",
+            "description": "<p>Id of the Type of the Question.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "DateTime",
+            "optional": false,
+            "field": "created_at",
+            "description": "<p>Creation date of the Question.</p>"
+          }
+        ]
+      }
+    },
+    "filename": "C:/projects/scouting/development/application/controllers/Question.php",
+    "groupTitle": "Question"
+  },
+  {
+    "type": "get",
+    "url": "/question/list",
+    "title": "Request Questions List",
+    "name": "QuestionGetList",
+    "group": "Question",
+    "version": "1.0.0",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "period_id",
+            "description": "<p>Id of the Period of the Questions.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "Number",
+            "optional": true,
+            "field": "question_type_id",
+            "description": "<p>Id of the Type of the Question.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": true,
+            "field": "search",
+            "description": "<p>Search string.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "questions",
+            "description": "<p>List of Questions</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "questions.id",
+            "description": "<p>Id of the Team.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "questions.period_id",
+            "description": "<p>Id of the Period of the Questions.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "questions.description",
+            "description": "<p>Description of the Question.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "questions.question_type_id",
+            "description": "<p>Id of the Type of the Question.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "DateTime",
+            "optional": false,
+            "field": "questions.created_at",
+            "description": "<p>Creation date of the Question.</p>"
+          }
+        ]
+      }
+    },
+    "filename": "C:/projects/scouting/development/application/controllers/Question.php",
+    "groupTitle": "Question"
+  },
+  {
+    "type": "get",
     "url": "/team/:id",
     "title": "Request Team information",
     "name": "TeamGet",
