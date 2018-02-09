7b7e3419094d69280c7b3d825a203dad44a7c332
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-02-09 18:00:54 -0200

    Finish core controller

--- development/application/core/MY_Controller.php
+++ development/application/core/MY_Controller.php
@@ -3,24 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class MY_Controller extends CI_Controller {
     
-    private $data;
+    protected $data;
 
     public function __construct($token_needed = TRUE) {
         parent::__construct();
         
-        $this->load->model('token_model');
+        $this->load->helper('help_helper');
+        $this->load->model(array('token_model', 'user_model'));
+        $this->load->library('form_validation');
 
         $this->output->set_content_type('application/json', 'utf-8');
 
-        if ($this->input->server('Accept') !== 'application/json') {
+        if ($this->input->get_request_header('Accept') !== 'application/json') {
             $this->_exit(406);
         }
 
-        if ($token_needed && !$this->_check_token()) {
-            $this->_exit(401);
+        if ($token_needed) {
+            $this->_require_token();
         }
 
-        $data = json_decode($this->input->raw_input_stream);
+        $data = json_decode($this->input->raw_input_stream, true);
         if (json_last_error() !== JSON_ERROR_NONE) {
             $this->_exit(400);
         }
@@ -28,18 +30,29 @@ class MY_Controller extends CI_Controller {
         $this->data = $data;
     }
 
-    protected function _exit(int $status_code = 200) {
+    protected function _exit(int $status_code = 200) : void {
         $this->output->set_status_header($status_code);
         $this->output->_display();
         exit();
     }
 
-    protected function _check_token() {
+    protected function _has_valid_token() : bool {
         $token = $this->input->server('Token');
         if (!$token) {
             return false;
         }
         return $this->token_model->is_valid_token($token);
+    }
+
+    protected function _require_token() : void {
+        if (!$this->_has_valid_token()) {
+            $this->_exit(401);
+        }
+    }
+
+    protected function _output_validation_errors() : void {
+        $output = array('errors' => $this->form_validation->error_array());
+        echo json_encode($output);
     }
 }
 ?>
