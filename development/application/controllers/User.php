9ea546a9c0dcdfcb55689de70de785e223b44800
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-02-10 14:33:22 -0200

    Use native function array_fill_keys

--- development/application/controllers/User.php
+++ development/application/controllers/User.php
@@ -44,7 +44,7 @@ class User extends MY_Controller {
 		if ($this->data) {
 			$search = get_array_values($this->data, ['id', 'team_id', 'search']);
 		} else {
-			$search = ['id' => null, 'team_id' => null, 'search' => null];
+			$search = array_fill_keys(['id', 'team_id', 'search'], null);
 		}
 
 		$user_id = $this->token_model->get_user_id($this->token);
