79e628f99442a0d14aad429c64b552e1f6cc2d3a
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-02-15 17:16:06 -0200

    Correct test data

--- analysis/question_type.sql
+++ analysis/question_type.sql
@@ -1,46 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 15, 2018 at 07:06 PM
--- Server version: 5.7.18
--- PHP Version: 7.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `scouting`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `question_type`
---
-
-DROP TABLE IF EXISTS `question_type`;
-CREATE TABLE IF NOT EXISTS `question_type` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `question_type`
---
-
 INSERT INTO `question_type` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Boolean', '2018-02-14 16:28:40', NULL),
 (2, 'Positive Integer', '2018-02-14 16:28:40', NULL),
@@ -48,8 +5,3 @@ INSERT INTO `question_type` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (4, 'Phrase', '2018-02-14 16:28:40', NULL),
 (5, 'Text', '2018-02-14 16:28:40', NULL),
 (6, 'Seconds', '2018-02-14 16:28:40', NULL);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
