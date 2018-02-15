bebd36c0d58b623b944ffbfb052c08c8cea194e8
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-02-14 22:19:49 -0200

    Add end parenthesis to the documentation

--- development/application/controllers/Question_type.php
+++ development/application/controllers/Question_type.php
@@ -21,7 +21,7 @@ class Question_type extends MY_Controller {
 	 * | 2 | Positive Integer	| Integer from 0 to 1,000,000			|
 	 * | 3 | Percent			| Integer from 0 to 100					|
 	 * | 4 | Phrase				| Small string (max 100 characters)		|
-	 * | 5 | Text				| Big string (max 1000 characters		|
+	 * | 5 | Text				| Big string (max 1000 characters)		|
 	 * | 6 | Seconds			| 0 to 86,400 (60 * 60 * 24 or 1 day)	|
 	 * 
 	 * @apiParam {Number} [id] Id of the Question Type.
