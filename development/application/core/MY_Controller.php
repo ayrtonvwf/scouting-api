c8b832a4c5aee3046594702df13d371e534941f2
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-02-16 16:13:34 -0200

    Fix CORS allowed methods

--- development/application/core/MY_Controller.php
+++ development/application/core/MY_Controller.php
@@ -22,6 +22,7 @@ class MY_Controller extends CI_Controller {
 
         header('Content-Type: application/json');
         header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Headers: Token');
         header('Cache-Control: no-cache');
         
